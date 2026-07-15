9fb7e3575efafbea6a72c98df5e65613ce4ad62c
FireFoXz09 <89284738+FireFoXz09@users.noreply.github.com>
2026-07-15 14:45:30 -0400

    Add files via upload
    Premier envoi du site

---
+++ portfolio-kits-data.js
@@ -0,0 +1,200 @@
+const portfolioKits = [
+  {
+    "name": "Rebelles de Sorel-Tracy",
+    "years": "2024-2025",
+    "cover": "portfolio/portfolio-01.png",
+    "images": [
+      {
+        "title": "Introduction",
+        "local_path": "portfolio/portfolio-01.png"
+      },
+      {
+        "title": "Sélection des champions",
+        "local_path": "portfolio/portfolio-02.png"
+      },
+      {
+        "title": "Overlay dans la partie",
+        "local_path": "portfolio/portfolio-03.jpg"
+      },
+      {
+        "title": "Graphique de fin de partie",
+        "local_path": "portfolio/portfolio-04.png"
+      },
+      {
+        "title": "Horaire des parties",
+        "local_path": "portfolio/portfolio-05.png"
+      },
+      {
+        "title": "Prochaine partie à l'horaire",
+        "local_path": "portfolio/portfolio-06.png"
+      }
+    ]
+  },
+  {
+    "name": "Eternal Esport League",
+    "years": "2025-2026",
+    "cover": "portfolio/portfolio-07.jpg",
+    "images": [
+      {
+        "title": "Écran d'introduction",
+        "local_path": "portfolio/portfolio-07.jpg"
+      },
+      {
+        "title": "Affichage des animateurs",
+        "local_path": "portfolio/portfolio-08.jpg"
+      },
+      {
+        "title": "Écran de pause",
+        "local_path": "portfolio/portfolio-09.jpg"
+      },
+      {
+        "title": "Sélection des champions",
+        "local_path": "portfolio/portfolio-10.jpg"
+      },
+      {
+        "title": "Overlay dans le jeu",
+        "local_path": "portfolio/portfolio-11.jpg"
+      },
+      {
+        "title": "Écran de fin de diffusion",
+        "local_path": "portfolio/portfolio-12.jpg"
+      }
+    ]
+  },
+  {
+    "name": "Balado GG BIEN JOUÉ",
+    "years": "2021-2026",
+    "cover": "portfolio/portfolio-13.png",
+    "images": [
+      {
+        "title": "Affichage en introduction",
+        "local_path": "portfolio/portfolio-13.png"
+      },
+      {
+        "title": "Scène avec complément d'information",
+        "local_path": "portfolio/portfolio-14.png"
+      },
+      {
+        "title": "Affichage principale avec les invités",
+        "local_path": "portfolio/portfolio-15.png"
+      }
+    ]
+  },
+  {
+    "name": "Rebelles du Cégep de Sorel-Tracy",
+    "years": "2023-2024",
+    "cover": "portfolio/portfolio-16.png",
+    "images": [
+      {
+        "title": "Écran d'introduction",
+        "local_path": "portfolio/portfolio-16.png"
+      },
+      {
+        "title": "Affichage des commentateurs",
+        "local_path": "portfolio/portfolio-17.png"
+      },
+      {
+        "title": "Alignement de départ",
+        "local_path": "portfolio/portfolio-18.png"
+      },
+      {
+        "title": "Cartes de la partie",
+        "local_path": "portfolio/portfolio-19.png"
+      },
+      {
+        "title": "Selection des champions",
+        "local_path": "portfolio/portfolio-20.png"
+      },
+      {
+        "title": "Classement général",
+        "local_path": "portfolio/portfolio-21.png"
+      },
+      {
+        "title": "Écran dans le jeu (Overwatch)",
+        "local_path": "portfolio/portfolio-22.jpg"
+      },
+      {
+        "title": "Écran dans le jeu (League of Legends)",
+        "local_path": "portfolio/portfolio-23.jpg"
+      },
+      {
+        "title": "Statistiques d'après-match",
+        "local_path": "portfolio/portfolio-24.png"
+      },
+      {
+        "title": "Statistiques détaillées d'après-match",
+        "local_path": "portfolio/portfolio-25.png"
+      },
+      {
+        "title": "Prochaines parties",
+        "local_path": "portfolio/portfolio-26.png"
+      }
+    ]
+  },
+  {
+    "name": "Rebelles du Cégep de Sorel-Tracy",
+    "years": "2022-2023",
+    "cover": "portfolio/portfolio-27.png",
+    "images": [
+      {
+        "title": "Écran d'introduction",
+        "local_path": "portfolio/portfolio-27.png"
+      },
+      {
+        "title": "Affichage des descripteurs",
+        "local_path": "portfolio/portfolio-28.png"
+      },
+      {
+        "title": "Classement général",
+        "local_path": "portfolio/portfolio-29.jpg"
+      },
+      {
+        "title": "Sélection des champions",
+        "local_path": "portfolio/portfolio-30.jpg"
+      },
+      {
+        "title": "Affichage dans la partie",
+        "local_path": "portfolio/portfolio-31.jpg"
+      },
+      {
+        "title": "Statistiques d'après-match",
+        "local_path": "portfolio/portfolio-32.jpg"
+      }
+    ]
+  },
+  {
+    "name": "Ligue Secondaire de Sports Électroniques (LSSE)",
+    "years": "2025-2026",
+    "cover": "portfolio/portfolio-33.png",
+    "images": [
+      {
+        "title": "Introduction de la diffusion",
+        "local_path": "portfolio/portfolio-33.png"
+      },
+      {
+        "title": "Affichage des descripteurs",
+        "local_path": "portfolio/portfolio-34.png"
+      },
+      {
+        "title": "Liste des cartes jouées",
+        "local_path": "portfolio/portfolio-35.png"
+      },
+      {
+        "title": "Affichage dans le jeu (Overwatch)",
+        "local_path": "portfolio/portfolio-36.png"
+      },
+      {
+        "title": "Sélection des champions",
+        "local_path": "portfolio/portfolio-37.jpg"
+      },
+      {
+        "title": "Affichage dans le jeu (League of Legends)",
+        "local_path": "portfolio/portfolio-38.jpg"
+      },
+      {
+        "title": "Écran de victoire",
+        "local_path": "portfolio/portfolio-39.png"
+      }
+    ]
+  }
+];
