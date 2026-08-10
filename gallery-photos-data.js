a8ff9f06e58f1bad34c26f9a1d3997a3be7e4d2f
FireFoXz09 <89284738+FireFoXz09@users.noreply.github.com>
2026-08-10 14:18:14 -0400

    Add files via upload
    maj pour galerie simplifié

---
+++ gallery-photos-data.js
@@ -0,0 +1,146 @@
+const galleryPhotos = [
+  {
+    "src": "gallery/photo-01.jpg",
+    "caption": "Opération LoL Québec - Mars 2014"
+  },
+  {
+    "src": "gallery/photo-02.jpg",
+    "caption": "Opération LoL Quebec - 2015"
+  },
+  {
+    "src": "gallery/photo-03.jpg",
+    "caption": "DFG LAN au Gaming Expo de Sherbrooke - 2015"
+  },
+  {
+    "src": "gallery/photo-04.jpg",
+    "caption": "Match amical universitaire présenté au Nexus Smart Bar à Montréal - 2016"
+  },
+  {
+    "src": "gallery/photo-05.jpg",
+    "caption": "Finales du Lan ETS - 2016"
+  },
+  {
+    "src": "gallery/photo-06.jpg",
+    "caption": "Table d'analyse pour les 7ème championnat du monde présenté sur RDS2 - 2016"
+  },
+  {
+    "src": "gallery/photo-07.jpg",
+    "caption": "Dreamhack Canada - 2016"
+  },
+  {
+    "src": "gallery/photo-08.jpg",
+    "caption": "vovlan au Cégep de Sorel-Tracy - 2010"
+  },
+  {
+    "src": "gallery/photo-09.jpg",
+    "caption": "Finales du tournoi universitaire de League of Legends QI Challenge - 2015"
+  },
+  {
+    "src": "gallery/photo-10.jpg",
+    "caption": "Analyse des finales du Lan ETS présenté par RDS - 2017"
+  },
+  {
+    "src": "gallery/photo-11.jpg",
+    "caption": "Finales du Lan ETS - 2018"
+  },
+  {
+    "src": "gallery/photo-12.jpg",
+    "caption": "Lancement de Jeux.ca - 2016"
+  },
+  {
+    "src": "gallery/photo-13.jpg",
+    "caption": "Événement caritatif pour la Campagne Cyber Espoir au Gaming Café de Montréal - 2017"
+  },
+  {
+    "src": "gallery/photo-14.jpg",
+    "caption": "Diffuseur invité au Dreamhack Montréal - 2017"
+  },
+  {
+    "src": "gallery/photo-15.jpg",
+    "caption": ""
+  },
+  {
+    "src": "gallery/photo-16.jpg",
+    "caption": "Animation devant public des parties des Rebelles du Cégep de Sorel-Tracy - 2021"
+  },
+  {
+    "src": "gallery/photo-17.jpg",
+    "caption": "Première victoire en tournoi sur League of Legends - 2012"
+  },
+  {
+    "src": "gallery/photo-18.jpg",
+    "caption": "Entrevue lors du Lan ETS avec LaGamacherie - 2020"
+  },
+  {
+    "src": "gallery/photo-19.jpg",
+    "caption": "Match amical lors de l'ouverture du Meltdown Montreal - 2016"
+  },
+  {
+    "src": "gallery/photo-20.jpg",
+    "caption": "Finales du tournoi universitaire de League of Legends QI Challenge - 2015"
+  },
+  {
+    "src": "gallery/photo-21.jpg",
+    "caption": "Analyse des finales du Lan ETS présenté par RDS - 2017"
+  },
+  {
+    "src": "gallery/photo-22.jpg",
+    "caption": "Gala Esports Québec (Prix pour la meilleure diffusion académique) - Novembre 2023"
+  },
+  {
+    "src": "gallery/photo-23.png",
+    "caption": "Match du 50ème anniversaire des Rebelles du Cégep de Sorel-Tracy - Février 2024"
+  },
+  {
+    "src": "gallery/photo-24.png",
+    "caption": "Finales du Lan ETS - 2016"
+  },
+  {
+    "src": "gallery/photo-25.jpg",
+    "caption": ""
+  },
+  {
+    "src": "gallery/photo-26.jpg",
+    "caption": "Finales du tournoi universitaire de League of Legends QI Challenge - 2015"
+  },
+  {
+    "src": "gallery/photo-27.jpg",
+    "caption": "Casters de League of Legends lors du Dreamhack Canada - 2016"
+  },
+  {
+    "src": "gallery/photo-28.jpg",
+    "caption": "Match amical en direct du Comiccom de Montréal - 2016"
+  },
+  {
+    "src": "gallery/photo-29.jpg",
+    "caption": "Équipe de diffusion au Lan ETS à la Place Bonaventure - 2016"
+  },
+  {
+    "src": "gallery/photo-30.jpg",
+    "caption": "Sur le tournage des championnats du monde de esports dans les studios de RDS - 2016"
+  },
+  {
+    "src": "gallery/photo-31.png",
+    "caption": "Casters au Lan ETS au Palais des Congrès - 2024"
+  },
+  {
+    "src": "gallery/photo-32.jpg",
+    "caption": "Équipe média pour Jeux.ca lors du Dreamhack Canada au Stade Olympique - 2018"
+  },
+  {
+    "src": "gallery/photo-33.jpg",
+    "caption": "Zone de diffusion de Dreamhack Canada - 2017"
+  },
+  {
+    "src": "gallery/photo-34.png",
+    "caption": "Finales du Lan ETS - 2020"
+  },
+  {
+    "src": "gallery/photo-35.jpg",
+    "caption": ""
+  },
+  {
+    "src": "gallery/photo-36.jpg",
+    "caption": "Trackmania au Lan ETS - 2025"
+  }
+];
