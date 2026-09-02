2292559025775c477a76c03eb47e1fb15892b2df
FireFoXz09 <89284738+FireFoXz09@users.noreply.github.com>
2026-09-02 11:15:12 -0400

    Add files via upload
    ajout page descripteur

---
+++ casting-videos-data.js
@@ -0,0 +1,7 @@
+const castingVideos = [
+  {
+    "title": "Démo description de hockey sur glace (2026)",
+    "category": "Hockey",
+    "embed_url": "https://www.youtube.com/embed/chCUQI5VwNk"
+  }
+];
